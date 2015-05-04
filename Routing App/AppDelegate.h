71788f635bdb62d5b5ce7bbfc6e37c0f15abef89
TacktileSystemsMac4 <tacktilesystemsmac4@Nandkishor.local>
2015-05-04 15:43:54 +0530

    Initial Commit

---
+++ Routing App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Routing App
+//
+//  Created by TacktileSystemsMac4 on 04/05/15.
+//  Copyright (c) 2015 Tacktile Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Routing App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Routing App
+//
+//  Created by TacktileSystemsMac4 on 04/05/15.
+//  Copyright (c) 2015 Tacktile Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
